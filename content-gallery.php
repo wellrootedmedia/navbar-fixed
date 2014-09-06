c1b22413baf271f71022d058d24dd1f2074953f2
Shawn Nolan <snolan760@shawnnolan.com>
2014-09-06 14:51:31 -0700

    Updated twitter hashtag for sharing.

--- content-gallery.php
+++ content-gallery.php
@@ -11,7 +11,7 @@
 
     <div class="social-sharing">
         <div class="twitter share">
-            <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo wp_get_shortlink(); ?>" data-via="" data-hashtags="ShawnNolanPhotography">Tweet</a>
+            <a href="https://twitter.com/share" class="twitter-share-button" data-url="<?php echo wp_get_shortlink(); ?>" data-via="" data-hashtags="ShawnNolanDotCom">Tweet</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
         </div>
         <div class="shortlink share">
